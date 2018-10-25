71c46bc5bd09c37ddac194e52ca84a2d9ef67d56
MonicaBarutia <monica.barutia@freshbyteinc.com>
2018-10-25 13:39:56 +0300

    Services: logger

--- the_spacebar/skeleton/src/Controller/ArticleController.php
+++ the_spacebar/skeleton/src/Controller/ArticleController.php
@@ -8,6 +8,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -46,9 +47,11 @@ class ArticleController extends  AbstractController
     /**
      * @Route("/news/{slug}/heart", name="article_toggle_heart", methods={"POST"})
      */
-    public function toggleArticleHeart($slug)
+    public function toggleArticleHeart($slug, LoggerInterface $logger)
     {
         //TODO - actually heart/unheart the article!
+
+        $logger->info('Article is being hearted!');
 
         return $this->json(['hearts'=>rand(5,100)]);
         //return new JsonResponse(['hearts'=>rand(5,100)]);
