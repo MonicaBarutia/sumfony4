6cf1bab4ee09195a165320ba64d08cbb8834aa99
MonicaBarutia <monica.barutia@freshbyteinc.com>
2018-10-26 12:03:55 +0300

    Set different cache for dev other than prod

--- the_spacebar/skeleton/src/Controller/ArticleController.php
+++ the_spacebar/skeleton/src/Controller/ArticleController.php
@@ -54,12 +54,12 @@ cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim ca
 fugiat.
 EOF;
 
+        //dump($cache);die;
         $item = $cache->getItem('markdown_'.md5($articleContent));
         if(!$item->isHit()) {
             $item->set($markdown->transform($articleContent));
             $cache->save($item);
         }
-
         $articleContent = $item->get();
 
 
