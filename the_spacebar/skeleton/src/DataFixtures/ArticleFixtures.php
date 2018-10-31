99141f8cfbc568ea6093b58aa00e98a25dc528e8
MonicaBarutia <monica.barutia@freshbyteinc.com>
2018-10-31 17:11:41 +0200

    Saving a ManyToMany Relation

--- the_spacebar/skeleton/src/DataFixtures/ArticleFixtures.php
+++ the_spacebar/skeleton/src/DataFixtures/ArticleFixtures.php
@@ -4,10 +4,12 @@ namespace App\DataFixtures;
 
 use App\Entity\Article;
 use App\Entity\Comment;
+use App\Entity\Tag;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 
-class ArticleFixtures extends BaseFixtures
+class ArticleFixtures extends BaseFixtures implements DependentFixtureInterface
 {
     private static $articleTitles = [
         'Why Asteroids Taste Like Bacon',
@@ -55,7 +57,26 @@ EOF
                 ->setHeartCount($this->faker->numberBetween(5, 100))
                 ->setImageFilename($this->faker->randomElement(self::$articleImages));
 
+            /**
+             * @var Tag[] $tags
+             */
+            $tags = $this->getRandomReferences(Tag::class, $this->faker->numberBetween(0, 5));
+            foreach ($tags as $tag){
+                $article->addTag($tag);
+            }
+
+
         });
         $manager->flush();
     }
+
+    public function getDependencies()
+
+    {
+        return [
+            TagFixture::class,
+        ];
+    }
+
+
 }
