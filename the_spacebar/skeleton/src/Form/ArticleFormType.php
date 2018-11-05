f4e04b017d9604fd2d819541f7f425184329a138
MonicaBarutia <monica.barutia@freshbyteinc.com>
2018-11-05 15:26:59 +0200

    Bind the Form to a Class

--- the_spacebar/skeleton/src/Form/ArticleFormType.php
+++ the_spacebar/skeleton/src/Form/ArticleFormType.php
@@ -9,8 +9,10 @@
 namespace App\Form;
 
 
+use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class ArticleFormType extends AbstractType
 {
@@ -19,6 +21,13 @@ class ArticleFormType extends AbstractType
         $builder
             ->add('title')
             ->add('content');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+       $resolver->setDefaults([
+            'data_class' => Article::class
+       ]);
     }
 
 }
